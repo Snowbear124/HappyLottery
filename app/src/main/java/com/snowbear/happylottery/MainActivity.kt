dcc3367d5f3b4672955863e7285fb0dc94e9c8b0
Snowbear124 <68908357+Snowbear124@users.noreply.github.com>
2021-11-06 22:10:51 +0800

    Second screen button and switch set.

--- app/src/main/java/com/snowbear/happylottery/MainActivity.kt
+++ app/src/main/java/com/snowbear/happylottery/MainActivity.kt
@@ -2,10 +2,23 @@ package com.snowbear.happylottery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    //也可以直接用"MainActivity"字串，但用class這個方式不占記憶體空間，會比較好
+    val TAG = MainActivity::class.java.simpleName
+    val testList = listOf<String>("Apply","Banana","Cherry","Dragonfruit","Fig")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.Frist_main)
+//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.set)
+    }
+
+    fun lottery(view: View) {
+        val item = testList.random()
+//        println("Item: $item")  //系統內部輸出的值
+        Log.d(TAG,"Lottery Item = $item")    //標籤，這裡做debug用
     }
 }
